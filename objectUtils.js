436cb117e8ccf8283af973a6dd99429c67a05ab5
Mircea Badragan <badraganmircea@gmail.com>
2019-02-24 19:27:15 +0200

    find root object by path implementation and UT

--- objectUtils.js
+++ objectUtils.js
@@ -1,60 +1,77 @@
 const getObj = (path, obj) => path.split('.').reduce((res, key) => res[key], obj);
 const getSourceKey = (sourcePath) => sourcePath.split('.')[sourcePath.split('.').length - 1];
 
-Object.prototype.addToKey = function (destinationPath, object) {
-    const destinationObj = getObj(destinationPath, this);
-    const destinationKey = Object.keys(object)[0];
-    destinationObj[destinationKey] = {...object[destinationKey]};
-};
+Object.prototype.findRootObject = function(options) {
+  const {
+    path,
+    withProperties
+  } = options;
 
-Object.prototype.deleteKey = function (sourcePath) {
-    const sourceKey = getSourceKey(sourcePath);
-    const sourcePathList = sourcePath.split('.');
-    sourcePathList.splice(-1, 1);
-    const sourceObjectPath = sourcePathList.join('.');
-    const sourceObject = getObj(sourceObjectPath, this);
-    delete sourceObject[sourceKey];
-};
-
-Object.prototype.moveKey = function (sourcePath, destinationPath) {
-    const source = getObj(sourcePath, this);
-    const destination = getObj(destinationPath, this);
-
-    const sourceKey = getSourceKey(sourcePath);
-    destination[sourceKey] = source;
-
-    this.deleteKey(sourcePath);
-};
-
-Object.prototype.renameKey = function (oldKey, newKey) {
-    if (oldKey === newKey) {
-        return this;
+  if (path) {
+    try {
+      return getObj(path, this);
+    } catch (e) {
+      return;
     }
-    if (this.hasOwnProperty(oldKey)) {
-        this[newKey] = this[oldKey];
-        delete this[oldKey];
-    }
+  }
+};
+
+Object.prototype.addToKey = function(destinationPath, object) {
+  const destinationObj = getObj(destinationPath, this);
+  const destinationKey = Object.keys(object)[0];
+  destinationObj[destinationKey] = {
+    ...object[destinationKey]
+  };
+};
+
+Object.prototype.deleteKey = function(sourcePath) {
+  const sourceKey = getSourceKey(sourcePath);
+  const sourcePathList = sourcePath.split('.');
+  sourcePathList.splice(-1, 1);
+  const sourceObjectPath = sourcePathList.join('.');
+  const sourceObject = getObj(sourceObjectPath, this);
+  delete sourceObject[sourceKey];
+};
+
+Object.prototype.moveKey = function(sourcePath, destinationPath) {
+  const source = getObj(sourcePath, this);
+  const destination = getObj(destinationPath, this);
+
+  const sourceKey = getSourceKey(sourcePath);
+  destination[sourceKey] = source;
+
+  this.deleteKey(sourcePath);
+};
+
+Object.prototype.renameKey = function(oldKey, newKey) {
+  if (oldKey === newKey) {
     return this;
+  }
+  if (this.hasOwnProperty(oldKey)) {
+    this[newKey] = this[oldKey];
+    delete this[oldKey];
+  }
+  return this;
 };
 
-Object.prototype.renameKeys = function (oldKey, newKey) {
-    if (oldKey === newKey) {
-        return this;
-    }
+Object.prototype.renameKeys = function(oldKey, newKey) {
+  if (oldKey === newKey) {
+    return this;
+  }
 
-    const iterate = (obj) => {
-        const objKeys = Object.keys(obj);
-        objKeys.forEach(property => {
-            if (obj.hasOwnProperty(property)) {
-                if (property === oldKey) {
-                    obj.renameKey(oldKey, newKey);
-                }
-                if (typeof obj[property] === "object") {
-                    iterate(obj[property]);
-                }
-            }
-        });
-    };
+  const iterate = (obj) => {
+    const objKeys = Object.keys(obj);
+    objKeys.forEach(property => {
+      if (obj.hasOwnProperty(property)) {
+        if (property === oldKey) {
+          obj.renameKey(oldKey, newKey);
+        }
+        if (typeof obj[property] === "object") {
+          iterate(obj[property]);
+        }
+      }
+    });
+  };
 
-    iterate(this);
+  iterate(this);
 };
--- test/objectUtils.test.js
+++ test/objectUtils.test.js
@@ -1,7 +1,67 @@
 import '../objectUtils';
 
-describe('add object utils', () => {
-  it('should add an object', () => {
-    expect(1).toBe(1);
+describe('find root object suite', () => {
+  it('finds object pointer at path', () => {
+    const object = {
+      a: {
+        b: 'c'
+      },
+      e: [1, 2, 3]
+    }
+
+    expect(object.findRootObject({
+      path: 'a.b'
+    })).toBe(object.a.b);
+    expect(object.findRootObject({
+      path: 'a'
+    })).toBe(object.a);
+    expect(object.findRootObject({
+      path: 'e'
+    })).toBe(object.e);
+  });
+
+  it('doesn\'t find object pointer if not existent', () => {
+    const object = {
+      a: {
+        b: 'c',
+        d: {
+          f: []
+        }
+      },
+      g: {}
+    }
+
+    expect(object.findRootObject({
+      path: 'a.b.d'
+    })).toBe(undefined);
+    expect(object.findRootObject({
+      path: 'a.e'
+    })).toBe(undefined);
+    expect(object.findRootObject({
+      path: 'a.c.d'
+    })).toBe(undefined);
+  });
+
+  it('finds correct object inside another object', () => {
+    const object = {
+      a: {
+        b: 'c',
+        d: {
+          f: []
+        }
+      },
+      g: {
+        h: {}
+      }
+    }
+    const insideObject = object.findRootObject({
+      path: 'a.d'
+    });
+    expect(insideObject.findRootObject({
+      path: 'f'
+    })).toBe(object.a.d.f);
+    expect(insideObject.findRootObject({
+      path: 'f'
+    })).toBe(insideObject.f);
   });
 });
